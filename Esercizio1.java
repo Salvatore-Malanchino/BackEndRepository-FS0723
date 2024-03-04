b42cb90a6defd3ea914cc035d27807894117bc8b
Salvatore-Malanchino <malanchinos@hotmail.it>
2024-03-04 16:15:59 +0100

    Nuovo branch + esercizi

---
+++ Esercizio1.java
@@ -0,0 +1,17 @@
+public class Esercizio1 {
+
+    public static int multiplication( int num1 , int num2){
+      return num1 * num2;
+
+    };
+
+public static String concatena(){
+
+}
+    public static void main(String[] args) {
+
+System.out.println(multiplication(5,9));
+    }
+
+
+}
---
+++ Esercizio3.java
@@ -0,0 +1,6 @@
+public class Esercizio3 {
+    public static void main(String[] args) {
+
+
+    }
+}
