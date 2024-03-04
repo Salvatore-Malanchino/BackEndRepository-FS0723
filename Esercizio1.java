8e0d7439d7c6be63a34bccc18db723a41870ba29
Salvatore-Malanchino <malanchinos@hotmail.it>
2024-03-04 16:54:17 +0100

    Esercizio 2 (nonn completamente funzionante)

--- Esercizio1.java
+++ Esercizio1.java
@@ -5,12 +5,16 @@ public class Esercizio1 {
 
     };
 
-public static String concatena(){
+public static String concatena (String str, int num2){
+    return str + num2;
 
 }
+
+String[] testArray= {"Uno","Due","Tre","Quattro","Cinque"};
     public static void main(String[] args) {
 
 System.out.println(multiplication(5,9));
+System.out.println(concatena("ciao ", 3));
     }
 
 
--- Esercizio2.java
+++ Esercizio2.java
@@ -1,5 +1,13 @@
-public class Esercizio2 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Esercizio2 {
+
+    public static void main(String[] args) {
+        Scanner testScanner= new Scanner(System.in);
+        System.out.print("Inserisci il tuo nome:");
+        String name= testScanner.nextLine();
+        System.out.print("User:"+ name);
+        int age= Integer.parseInt(testScanner.nextLine());
+        System.out.print("Age:"+ age);
     }
 }
